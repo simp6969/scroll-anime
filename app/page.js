aacb0d07a5314121c35831b735e34e833a396edf
bold ariunbold <ariunboldbold200@gmail.com>
2024-01-29 05:29:43 +0800

    added second ref

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function MainPage() {
   const myRef = useRef();
+  const mySecondRef = useRef();
   const [opacity, setOpacity] = useState(0);
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -15,16 +17,32 @@ export default function MainPage() {
       }
     });
     observer.observe(myRef.current);
+    observer.observe(mySecondRef.current);
   }, []);
   return (
-    <div className="flex h-[300vh] w-[100vw] items-center justify-center">
+    <div className="flex h-[300vh] w-[100vw] items-center justify-around flex-col gap-[50px]">
       <h1
         ref={myRef}
-        style={{ opacity: opacity, scale: opacity + 0.3 }}
+        style={{
+          opacity: opacity,
+          transform: "translateY(" + opacity * 40 + "px)",
+        }}
         className="anime"
       >
         i cant find the lorem text :/
       </h1>
+      <Image
+        ref={mySecondRef}
+        style={{
+          opacity: opacity,
+          transform: "translateX(" + opacity * 40 + "px)",
+        }}
+        height="200"
+        width="150"
+        src="/bg.jpg"
+        className="photo"
+        alt="idk"
+      ></Image>
     </div>
   );
 }
